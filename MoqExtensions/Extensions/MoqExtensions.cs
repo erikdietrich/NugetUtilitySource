4204a77e16e94cb88202cee50e780c1639125d11
Erik Dietrich <erik@daedtech.com>
2013-03-04 14:10:15 -0600

    Created

---
+++ MoqExtensions/Extensions/MoqExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Moq;
+using Moq.Language.Flow;
+
+namespace NugetUtilitySource.MoqExtensions.Extensions
+{
+    public static class MoqExtensions
+    {
+        public static void ReturnsInOrder<T, TResult>(this ISetup<T, TResult> setup, params TResult[] results) where T : class
+        {
+            setup.Returns(new Queue<TResult>(results).Dequeue);
+        }
+
+        public static ISetup<T, TResult> Setup<T, TResult>(this T mock, Expression<Func<T, TResult>> expression) where T : class
+        {
+            return Mock.Get(mock).Setup(expression);
+        }
+
+        public static ISetup<T> Setup<T>(this T mock, Expression<Action<T>> expression) where T : class
+        {
+            return Mock.Get(mock).Setup(expression);
+        }
+
+        public static void Verify<T, TResult>(this T mock, Expression<Func<T, TResult>> expression) where T : class
+        {
+            Mock.Get(mock).Verify(expression);
+        }
+
+        public static void Verify<T>(this T mock, Expression<Action<T>> expression, Times times) where T : class
+        {
+            Mock.Get(mock).Verify(expression, times);
+        }
+
+        public static void Verify<T>(this T mock, Expression<Action<T>> expression) where T : class
+        {
+            Mock.Get(mock).Verify(expression);
+        }
+
+        public static void Verify<T, TResult>(this T mock, Expression<Func<T, TResult>> expression, Times times) where T : class
+        {
+            Mock.Get(mock).Verify(expression, times);
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this T item)
+        {
+            yield return item;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this T item, int count)
+        {
+            for (int index = 0; index < count; index++)
+                yield return item;
+        }
+    }
+}
---
+++ PrimitiveExtensions/Extensions/PrimitiveExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NugetUtilitySource.PrimitiveExtensions.Extensions
+{
+    public static class PrimitiveExtensions
+    {
+        public static int ParseIntOrDefault(this string target)
+        {
+            int dummy = 0;
+            return int.TryParse(target, out dummy) ? dummy : 0;
+        }
+
+        public static int ParseIntOrDefault(this object target)
+        {
+            return target == null ? 0 : ParseIntOrDefault(target.ToString());
+        }
+
+        public static double ParseDoubleOrDefault(this string target)
+        {
+            double dummy = 0;
+            return double.TryParse(target, out dummy) ? dummy : 0;
+        }
+
+        public static double ParseDoubleOrDefault(this object target)
+        {
+            return target != null ? 0 : ParseDoubleOrDefault(target);
+        }
+    }
+}
